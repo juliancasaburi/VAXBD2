73853264a2ef479c23acfa2d70174726b43b75a5
Julian Casaburi <juliancasaburi@gmail.com>
2022-05-17 01:48:29 -0300

    Modificar el username de mysql en script y config

--- sql_grupo3.sql
+++ sql_grupo3.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE `grupo3` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-create USER 'grupo2022'@'localhost' IDENTIFIED WITH mysql_native_password BY 'pa$$word';
-GRANT ALL PRIVILEGES on grupo3.* TO 'grupo2022'@'localhost';
+create USER '2022bd2grupo3'@'localhost' IDENTIFIED WITH mysql_native_password BY 'pa$$word';
+GRANT ALL PRIVILEGES on grupo3.* TO '2022bd2grupo3'@'localhost';
