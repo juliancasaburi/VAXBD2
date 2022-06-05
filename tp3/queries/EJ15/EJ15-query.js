0525e07c4539e309cecad80f56df16f9374127d1
Geronimo Boza <geronimoboza@gmail.com>
2022-06-05 19:52:41 -0300

    correccion de ejercicio 15; correccion de pdf

--- tp3/queries/EJ15/EJ15-query.js
+++ tp3/queries/EJ15/EJ15-query.js
@@ -2,11 +2,12 @@ use vaccination;
 db.patients.aggregate([
    {
      $geoNear: {
-        near: { type: "MultiPolygon", coordinates: [-58.4586,-34.5968] },
+        near: { type: "Point", coordinates: [-58.4586,-34.5968] },
         distanceField: "distance.meters",
         maxDistance: 1000,
         spherical: true,
      }
     },   
+    { $project: {"distance": 0}},
     { $out : "cabaPatients" }   
 ])
