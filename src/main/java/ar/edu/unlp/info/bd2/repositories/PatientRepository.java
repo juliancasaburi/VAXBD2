1919364735e122c0a084b7210fb8ca52214ab51e
Julian Casaburi <juliancasaburi@gmail.com>
2022-06-15 20:43:03 -0300

    Agregar SpringDataVaxService con createPatient y getPatientByEmail (testCreatePatient pasando)

--- src/main/java/ar/edu/unlp/info/bd2/repositories/PatientRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/PatientRepository.java
@@ -4,6 +4,16 @@ import ar.edu.unlp.info.bd2.model.Patient;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientRepository extends CrudRepository<Patient, Long> {
+
+    /**
+     * Recupera el Patient con email email.
+     *
+     * @param email
+     * @return el TestModel cuyo title contiene title.
+     */
+    Optional<Patient> findPatientByEmail(String email);
 }
---
+++ src/main/java/ar/edu/unlp/info/bd2/services/SpringDataVaxService.java
@@ -0,0 +1,178 @@
+package ar.edu.unlp.info.bd2.services;
+
+import ar.edu.unlp.info.bd2.model.*;
+import ar.edu.unlp.info.bd2.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class SpringDataVaxService implements VaxService {
+
+    /* Repositories */
+    @Autowired
+    private CentreRepository centreRepository;
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Autowired
+    private NurseRepository nurseRepository;
+
+    @Autowired
+    private ShotCertificateRepository shotCertificateRepository;
+
+    @Autowired
+    private ShotRepository shotRepository;
+
+    @Autowired
+    private StaffRepository staffRepository;
+
+    @Autowired
+    private SupportStaffRepository supportStaffRepository;
+
+    @Autowired
+    private VaccinationScheduleRepository vaccinationScheduleRepository;
+
+    @Autowired
+    private VaccineRepository vaccineRepository;
+
+    /* TP1 Methods */
+
+    @Transactional
+    @Override
+    public Patient createPatient(String email, String fullname, String password, Date dayOfBirth) throws VaxException {
+        Patient patient = new Patient(email, password, fullname, dayOfBirth);
+        try {
+            patientRepository.save(patient);
+            return patient;
+        } catch (DataIntegrityViolationException e) {
+            throw new VaxException("Constraint Violation");
+        }
+    }
+
+    @Transactional
+    @Override
+    public Vaccine createVaccine(String name) throws VaxException {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public Shot createShot(Patient patient, Vaccine vaccine, Date date, Centre centre, Nurse nurse) throws VaxException {
+        return null;
+    }
+
+    @Override
+    public Optional<Patient> getPatientByEmail(String email) {
+        return this.patientRepository.findPatientByEmail(email);
+    }
+
+    @Override
+    public Optional<Vaccine> getVaccineByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Centre createCentre(String name) throws VaxException {
+        return null;
+    }
+
+    @Override
+    public Nurse createNurse(String dni, String fullName, Integer experience) throws VaxException {
+        return null;
+    }
+
+    @Override
+    public SupportStaff createSupportStaff(String dni, String fullName, String area) throws VaxException {
+        return null;
+    }
+
+    @Override
+    public VaccinationSchedule createVaccinationSchedule() throws VaxException {
+        return null;
+    }
+
+    @Override
+    public VaccinationSchedule getVaccinationScheduleById(Long id) throws VaxException {
+        return null;
+    }
+
+    @Override
+    public Optional<Centre> getCentreByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public SupportStaff updateSupportStaff(SupportStaff staff) throws VaxException {
+        return null;
+    }
+
+    @Override
+    public Centre updateCentre(Centre centre) throws VaxException {
+        return null;
+    }
+
+    @Override
+    public Optional<SupportStaff> getSupportStaffByDni(String dni) {
+        return Optional.empty();
+    }
+
+    /* TP2 Methods */
+
+    @Override
+    public List<Patient> getAllPatients() {
+        return null;
+    }
+
+    @Override
+    public List<Nurse> getNurseWithMoreThanNYearsExperience(int years) {
+        return null;
+    }
+
+    @Override
+    public List<Centre> getCentresTopNStaff(int n) {
+        return null;
+    }
+
+    @Override
+    public Centre getTopShotCentre() {
+        return null;
+    }
+
+    @Override
+    public List<Nurse> getNurseNotShot() {
+        return null;
+    }
+
+    @Override
+    public String getLessEmployeesSupportStaffArea() {
+        return null;
+    }
+
+    @Override
+    public List<Staff> getStaffWithName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Vaccine> getUnappliedVaccines() {
+        return null;
+    }
+
+    @Override
+    public List<ShotCertificate> getShotCertificatesBetweenDates(Date startDate, Date endDate) {
+        return null;
+    }
+
+    @Override
+    public VaccinationSchedule updateVaccinationSchedule(VaccinationSchedule vaccinationSchedule) throws VaxException {
+        return null;
+    }
+}
